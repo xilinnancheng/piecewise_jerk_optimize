fb11b80c46b9fe601318764480b226d5969e74df
chr <scut_chr@163.com>
2022-02-28 23:52:33 +0800

    version 0.1

---
+++ piecewise_jerk_optimize.py
@@ -0,0 +1,231 @@
+import math
+import numpy
+import osqp
+from scipy import sparse
+from matplotlib import pyplot as plt
+
+class PieceJerkOptimize:
+    def __init__(self, num_of_point):
+        self.num_of_point = num_of_point
+        self.num_of_variable = 3 * num_of_point
+
+        self.w_x = 0.0
+        self.w_dx = 0.0
+        self.w_ddx = 0.0
+        self.w_dddx = 0.0
+        self.w_ref_x = 0.0
+
+        self.step = []
+        self.step_square = []
+        self.ref_x = []
+        self.x_upper_bound = []
+        self.x_lower_bound = []
+        self.dx_upper_bound = []
+        self.dx_lower_bound = []
+        self.ddx_upper_bound = []
+        self.ddx_lower_bound = []
+        self.dddx_upper_bound = []
+        self.dddx_lower_bound = []
+        self.solution_x = []
+        self.solution_dx = []
+        self.solution_ddx = []
+        self.solution_dddx = []
+        pass
+
+    def SetWeight(self, w_x, w_dx,w_ddx, w_dddx, w_ref_x):
+        self.w_x = w_x
+        self.w_dx = w_dx
+        self.w_ddx = w_ddx
+        self.w_dddx = w_dddx
+        self.w_ref_x = w_ref_x
+
+    def SetReferenceX(self, ref_x):
+        self.ref_x = ref_x
+
+    def SetStep(self, step):
+        self.step = step
+        self.step_square = [x for x in self.step]
+
+    def SetXBound(self, upper_bound, lower_bound):
+        self.x_lower_bound = lower_bound
+        self.x_upper_bound = upper_bound
+
+    def SetDXBound(self, upper_bound, lower_bound):
+        self.dx_lower_bound = lower_bound
+        self.dx_upper_bound = upper_bound
+
+    def SetDDXBound(self, upper_bound, lower_bound):
+        self.ddx_lower_bound = lower_bound
+        self.ddx_upper_bound = upper_bound
+
+    def SetDDDXBound(self, upper_bound, lower_bound):
+        self.dddx_lower_bound = lower_bound
+        self.dddx_upper_bound = upper_bound
+
+    def CalculateQ(self):
+        row = []
+        col = []
+        data = []
+        return sparse.csc_matrix((data, (row, col)), shape=(
+            self.num_of_variable, self.num_of_variable))
+
+    def CalculateP(self):
+        P = []
+        for i in range(self.num_of_point):
+            P.append(-2.0 * self.ref_x[i] * self.w_ref_x)
+
+        for i in range(2 * self.num_of_point):
+            P.append(0.0)
+        
+        P = numpy.array(P)
+        return P
+
+    def CalculateAffineConstraint(self):
+        row = []
+        col = []
+        data = []
+
+        # x constrait.
+        for i in range(self.num_of_point):
+            row.append(i)
+            col.append(i)
+            data.append(1.0)
+        
+        # dx constraint.
+        for i in range(self.num_of_point):
+            row.append(self.num_of_point + i)
+            col.append(self.num_of_point + i)
+            data.append(1)
+
+        # ddx constraint.
+        for i in range(self.num_of_point):
+            row.append(2 * self.num_of_point + i)
+            col.append(2 * self.num_of_point + i)
+            data.append(1)
+        
+        # dddx constraint.
+        for i in range(self.num_of_point - 1):
+            row.append(3 * self.num_of_point + i)
+            col.append(2 * self.num_of_point + i)
+            data.append(-1.0)
+            row.append(3 * self.num_of_point + i)
+            col.append(2 * self.num_of_point + i + 1)
+            data.append(1.0)
+
+        # start/end x limit
+        row.append(4 * self.num_of_point - 1)
+        col.append(0)
+        data.append(1)
+
+        row.append(4 * self.num_of_point)
+        col.append(self.num_of_point - 1)
+        data.append(1)
+
+        # ddx consistency
+        for i in range(self.num_of_point - 1):
+            row.append(4 * self.num_of_point + 1 + i)
+            col.append(self.num_of_point + i)
+            data.append(1)
+
+            row.append(4 * self.num_of_point + 1 + i)
+            col.append(self.num_of_point + i + 1)
+            data.append(-1)
+
+            row.append(4 * self.num_of_point + 1 + i)
+            col.append(2 * self.num_of_point + i)
+            data.append(0.5 * self.step[i])
+
+            row.append(4 * self.num_of_point + 1 + i)
+            col.append(2 * self.num_of_point + i + 1)
+            data.append(0.5 * self.step[i])
+
+        # dx consistency
+        for i in range(self.num_of_point - 1):
+            row.append(5 * self.num_of_point + i)
+            col.append(i)
+            data.append(1)
+
+            row.append(5 * self.num_of_point + i)
+            col.append(i + 1)
+            data.append(-1)
+
+            row.append(5 * self.num_of_point + i)
+            col.append(self.num_of_point + i)
+            data.append(self.step[i])
+
+            row.append(5 * self.num_of_point + i)
+            col.append(self.num_of_point + i + 1)
+            data.append(self.step[i])
+
+            row.append(5 * self.num_of_point + i)
+            col.append(2 * self.num_of_point + i)
+            data.append(1.0/3.0 * self.step_square[i])
+
+            row.append(5 * self.num_of_point + i)
+            col.append(2 * self.num_of_point + i + 1)
+            data.append(1.0/6.0 * self.step_square[i])
+
+        A = sparse.csc_matrix((data, (row, col)), shape=(
+            6 * self.num_of_point - 1, self.num_of_variable))
+
+        lb = []
+        ub = []
+
+        # x bound
+        for i in range(self.num_of_point):
+            lb.append(self.x_lower_bound[i])
+            ub.append(self.x_upper_bound[i])
+        
+        # dx bound
+        for i in range(self.num_of_point):
+            lb.append(self.dx_lower_bound[i])
+            ub.append(self.dx_upper_bound[i])
+        
+        # ddx bound
+        for i in range(self.num_of_point):
+            lb.append(self.ddx_lower_bound[i])
+            ub.append(self.ddx_upper_bound[i])
+        
+        # dddx bound
+        for i in range(self.num_of_point - 1):
+            lb.append(self.dddx_lower_bound[i])
+            ub.append(self.dddx_upper_bound[i])
+
+        ## dx consistency
+        for i in range(self.num_of_point - 1):
+            lb.append(-10e-5)
+            ub.append(10e-5)
+        
+        ## x consistency
+        for i in range(self.num_of_point - 1):
+            lb.append(-10e-5)
+            ub.append(10e-5)
+
+        # start/end x
+        lb.append(self.ref_x[0] - 1e-5)
+        ub.append(self.ref_x[0] + 1e-5)
+        lb.append(self.ref_x[self.num_of_point-1] - 1e-5)
+        ub.append(self.ref_x[self.num_of_point-1] + 1e-5)
+
+        return A,lb,ub    
+
+    def Optimize(self):
+        Q = self.CalculateQ()
+        P = self.CalculateP()
+
+        A, lb, ub = self.CalculateAffineConstraint()
+
+        prob = osqp.OSQP()
+        prob.setup(Q, P, A, lb, ub, polish=True, eps_abs=1e-5, eps_rel=1e-5,
+                   eps_prim_inf=1e-5, eps_dual_inf=1e-5, verbose=True)
+
+        var_warm_start = numpy.array(self.ref_x + [0.0 for n in range(2 * self.num_of_point)])
+        prob.warm_start(x = var_warm_start)
+        res = prob.solve()
+
+        self.solution_x = res.x[0:self.num_of_point]
+        self.solution_dx = res.x[self.num_of_point:2 *self.num_of_point]
+        self.solution_ddx = res.x[2 *self.num_of_point:3 * self.num_of_point]
+
+    def VizResult(self):
+        pass
--- piecewise_jerk_path_optimize.py
+++ piecewise_jerk_path_optimize.py
@@ -3,43 +3,16 @@ import numpy
 import osqp
 from scipy import sparse
 from matplotlib import pyplot as plt
+from piecewise_jerk_optimize import PieceJerkOptimize
 
-class PieceJerkPathOptimize:
-    def __init__(self, ref_path_s, ref_path_upper_l, ref_path_lower_l, ref_path_kappa, w_l, w_dl, w_ddl, w_dddl, w_ref_l, wheel_base, max_delta, max_delta_rate):
-        self.num_of_point = len(ref_path_s)
-        self.num_of_variable = 3 * self.num_of_point
+class PieceJerkPathOptimize(PieceJerkOptimize):
+    def __init__(self, num_of_point):
+        super().__init__(num_of_point)
+        self.ref_path_s = []
+        
+    def SetReferencePathS(self, ref_path_s):
         self.ref_path_s = ref_path_s
-        self.ref_path_upper_l = ref_path_upper_l
-        self.ref_path_lower_l = ref_path_lower_l
-        self.ref_path_kappa = ref_path_kappa
-        self.w_l = w_l
-        self.w_dl = w_dl
-        self.w_ddl = w_ddl
-        self.w_dddl = w_dddl
-        self.w_ref_l = w_ref_l
-        self.wheel_base = wheel_base
-        self.max_delta = max_delta
-        self.max_delta_rate = max_delta_rate
-        self.s_step = []
-        self.s_step_square = []
-        self.ref_path_l = []
-        for i in range(len(ref_path_s) - 1):
-            delta_s = ref_path_s[i+1] - ref_path_s[i]
-            self.s_step.append(delta_s)
-            self.s_step_square.append(pow(delta_s, 2))
-        
-        for i in range(len(ref_path_s)):
-            self.ref_path_l.append(0.5 * (ref_path_upper_l[i] + ref_path_lower_l[i]))
-        
-        self.dl_upper_bound = []
-        self.dl_lower_bound = []
-        self.ddl_upper_bound = []
-        self.ddl_lower_bound = []
-        self.solution_l = []
-        self.solution_dl = []
-        self.solution_ddl = []
-        self.solution_dddl = []
-        
+
     def CalculateQ(self):
         row = []
         col = []
@@ -49,247 +22,88 @@ class PieceJerkPathOptimize:
         for i in range(self.num_of_point):
             row.append(i)
             col.append(i)
-            data.append(2.0 * self.w_l)
+            data.append(2.0 * self.w_x)
 
         # dl cost
         for i in range(self.num_of_point):
             row.append(self.num_of_point + i)
             col.append(self.num_of_point + i)
-            data.append(2.0 * self.w_dl)
+            data.append(2.0 * self.w_dx)
 
         # ddl cost
         for i in range(self.num_of_point):
             row.append(2 * self.num_of_point + i)
             col.append(2 * self.num_of_point + i)
-            data.append(2.0 * self.w_ddl)
+            data.append(2.0 * self.w_ddx)
         
         # dddl cost
         for i in range(self.num_of_point - 1):
             row.append(2 * self.num_of_point + i)
             col.append(2 * self.num_of_point + i)
-            data.append(data[2 * self.num_of_point + i] + self.w_dddl /self.s_step_square[i])
+            data.append(data[2 * self.num_of_point + i] + self.w_dddx /self.step_square[i])
             row.append(2 * self.num_of_point + i + 1)
             col.append(2 * self.num_of_point + i + 1)
-            data.append(data[2 * self.num_of_point + i + 1] + self.w_dddl /self.s_step_square[i])
+            data.append(data[2 * self.num_of_point + i + 1] + self.w_dddx /self.step_square[i])
             row.append(2 * self.num_of_point + i + 1)
             col.append(2 * self.num_of_point + i)
-            data.append(-2.0 * self.w_dddl /self.s_step_square[i])
+            data.append(-2.0 * self.w_dddx /self.step_square[i])
         
         row.append(2 * self.num_of_point + self.num_of_point - 1)
         col.append(2 * self.num_of_point + self.num_of_point - 1)
         data.append(data[2 * self.num_of_point + self.num_of_point - 1] /
-            + self.w_dddl /self.s_step_square[self.num_of_point - 2])
+            + self.w_dddx /self.step_square[self.num_of_point - 2])
 
         # l_ref cost
         for i in range(self.num_of_point):
-            data[i] += 2.0 * self.w_ref_l
+            data[i] += 2.0 * self.w_ref_x
         
         Q = sparse.csc_matrix((data, (row, col)), shape=(
             self.num_of_variable, self.num_of_variable))
         
         return Q
 
-    def CalculateP(self):
-        P = []
-        for i in range(self.num_of_point):
-            P.append(-2.0 * self.ref_path_l[i] * self.w_ref_l)
-
-        for i in range(2 * self.num_of_point):
-            P.append(0.0)
-        
-        P = numpy.array(P)
-        return P
-
-    def CalculateAffineConstraint(self):
-        row = []
-        col = []
-        data = []
-
-        # l constrait.
-        for i in range(self.num_of_point):
-            row.append(i)
-            col.append(i)
-            data.append(1.0)
-        
-        # dl constraint.
-        for i in range(self.num_of_point):
-            row.append(self.num_of_point + i)
-            col.append(self.num_of_point + i)
-            data.append(1)
-
-        # ddl constraint.
-        for i in range(self.num_of_point):
-            row.append(2 * self.num_of_point + i)
-            col.append(2 * self.num_of_point + i)
-            data.append(1)
-        
-        # dddl constraint.
-        for i in range(self.num_of_point - 1):
-            row.append(3 * self.num_of_point + i)
-            col.append(2 * self.num_of_point + i)
-            data.append(-1.0)
-            row.append(3 * self.num_of_point + i)
-            col.append(2 * self.num_of_point + i + 1)
-            data.append(1.0)
-
-        # start/end l limit
-        row.append(4 * self.num_of_point - 1)
-        col.append(0)
-        data.append(1)
-
-        row.append(4 * self.num_of_point)
-        col.append(self.num_of_point - 1)
-        data.append(1)
-
-        # ddl consistency
-        for i in range(self.num_of_point - 1):
-            row.append(4 * self.num_of_point + 1 + i)
-            col.append(self.num_of_point + i)
-            data.append(1)
-
-            row.append(4 * self.num_of_point + 1 + i)
-            col.append(self.num_of_point + i + 1)
-            data.append(-1)
-
-            row.append(4 * self.num_of_point + 1 + i)
-            col.append(2 * self.num_of_point + i)
-            data.append(0.5 * self.s_step[i])
-
-            row.append(4 * self.num_of_point + 1 + i)
-            col.append(2 * self.num_of_point + i + 1)
-            data.append(0.5 * self.s_step[i])
-
-        # dl consistency
-        for i in range(self.num_of_point - 1):
-            row.append(5 * self.num_of_point + i)
-            col.append(i)
-            data.append(1)
-
-            row.append(5 * self.num_of_point + i)
-            col.append(i + 1)
-            data.append(-1)
-
-            row.append(5 * self.num_of_point + i)
-            col.append(self.num_of_point + i)
-            data.append(self.s_step[i])
-
-            row.append(5 * self.num_of_point + i)
-            col.append(self.num_of_point + i + 1)
-            data.append(self.s_step[i])
-
-            row.append(5 * self.num_of_point + i)
-            col.append(2 * self.num_of_point + i)
-            data.append(1.0/3.0 * self.s_step[i] * self.s_step[i])
-
-            row.append(5 * self.num_of_point + i)
-            col.append(2 * self.num_of_point + i + 1)
-            data.append(1.0/6.0 * self.s_step[i] * self.s_step[i])
-
-        A = sparse.csc_matrix((data, (row, col)), shape=(
-            6 * self.num_of_point - 1, self.num_of_variable))
-
-        numpy.set_printoptions(linewidth=numpy.inf)
-        lb = []
-        ub = []
-        # l
-        for i in range(self.num_of_point):
-            lb.append(self.ref_path_lower_l[i])
-            ub.append(self.ref_path_upper_l[i])
-        
-        # dl = (1 - kappa * l) * tan(delta_theta)
-        for i in range(self.num_of_point):
-            self.dl_lower_bound.append(-math.tan(numpy.deg2rad(30)))
-            self.dl_upper_bound.append(math.tan(numpy.deg2rad(30)))
-            lb.append(-math.tan(numpy.deg2rad(30)))
-            ub.append(math.tan(numpy.deg2rad(30)))
-        
-        # ddl = tan(max_delta)/wheel_base - k_r
-        for i in range(self.num_of_point):
-            self.ddl_lower_bound.append(-(math.tan(self.max_delta)/self.wheel_base - 0.0))
-            self.ddl_upper_bound.append(math.tan(self.max_delta)/self.wheel_base - 0.0)
-            lb.append(-(math.tan(self.max_delta)/self.wheel_base - 0.0))
-            ub.append(math.tan(self.max_delta)/self.wheel_base - 0.0)
-        
-        # dddl
-        for i in range(self.num_of_point - 1):
-            lb.append(-self.max_delta_rate / self.wheel_base / 2.0)
-            ub.append(self.max_delta_rate / self.wheel_base / 2.0)
-
-        ## dl consistency
-        for i in range(self.num_of_point - 1):
-            lb.append(-10e-5)
-            ub.append(10e-5)
-        
-        ## l consistency
-        for i in range(self.num_of_point - 1):
-            lb.append(-10e-5)
-            ub.append(10e-5)
-
-        # start/end l
-        lb.append(self.ref_path_l[0] - 1e-5)
-        ub.append(self.ref_path_l[0] + 1e-5)
-        lb.append(self.ref_path_l[self.num_of_point-1] - 1e-5)
-        ub.append(self.ref_path_l[self.num_of_point-1] + 1e-5)
-
-        return A,lb,ub
-
-    def Optimize(self):
-        Q = self.CalculateQ()
-        P = self.CalculateP()
-
-        A, lb, ub = self.CalculateAffineConstraint()
-
-        prob = osqp.OSQP()
-        prob.setup(Q, P, A, lb, ub, polish=True, eps_abs=1e-5, eps_rel=1e-5,
-                   eps_prim_inf=1e-5, eps_dual_inf=1e-5, verbose=True)
-
-        var_warm_start = numpy.array(self.ref_path_l + [0.0 for n in range(2 * self.num_of_point)])
-        prob.warm_start(x = var_warm_start)
-        res = prob.solve()
-
-        self.solution_l = res.x[0:self.num_of_point]
-        self.solution_dl = res.x[self.num_of_point:2 *self.num_of_point]
-        self.solution_ddl = res.x[2 *self.num_of_point:3 * self.num_of_point]
-        
+    def VizResult(self):        
         plt.subplot(3, 1, 1)
-        plt.plot(self.ref_path_s, self.ref_path_upper_l,'r',marker="x")
-        plt.plot(self.ref_path_s, self.ref_path_lower_l,'r',marker="x")
-        plt.plot(self.ref_path_s, self.ref_path_l,'g',marker="x")
-        plt.plot(self.ref_path_s, self.solution_l,'b')
+        plt.plot(self.ref_path_s, self.x_upper_bound,'r',marker="x")
+        plt.plot(self.ref_path_s, self.x_lower_bound,'r',marker="x")
+        plt.plot(self.ref_path_s, self.ref_x,'g',marker="x")
+        plt.plot(self.ref_path_s, self.solution_x,'b')
         plt.grid()
         plt.legend(["upper_bound","lower_bound","ref_path_l","solution_l"])
         plt.title("PieceWise Jerk Path optimization")
 
         plt.subplot(3, 1, 2)
-        plt.plot(self.ref_path_s, self.solution_dl,'b')
-        plt.plot(self.ref_path_s, self.dl_upper_bound,'r')
-        plt.plot(self.ref_path_s, self.dl_lower_bound,'r')
+        plt.plot(self.ref_path_s, self.solution_dx,'b')
+        plt.plot(self.ref_path_s, self.dx_upper_bound,'r')
+        plt.plot(self.ref_path_s, self.dx_lower_bound,'r')
         plt.grid()
         plt.legend(["solution_dl","dl_bound"])
 
         plt.subplot(3, 1, 3)
-        plt.plot(self.ref_path_s, self.solution_ddl,'b')
-        plt.plot(self.ref_path_s, self.ddl_upper_bound,'r')
-        plt.plot(self.ref_path_s, self.ddl_lower_bound,'r')
+        plt.plot(self.ref_path_s, self.solution_ddx,'b')
+        plt.plot(self.ref_path_s, self.ddx_upper_bound,'r')
+        plt.plot(self.ref_path_s, self.ddx_lower_bound,'r')
         plt.legend(["solution_ddl","ddl_bound"])
         plt.grid()
         plt.show()
         
 if __name__ == "__main__":
-    # parameter
+    # weight parameter
     w_l = 5
     w_dl = 10
     w_ddl = 30
     w_dddl = 40
     w_ref_l = 100
+    
+    # car paramter
     wheel_base = 2.8
     max_delta = numpy.deg2rad(29.375)
     max_delta_rate = numpy.deg2rad(31.25)
 
     # Reference path
-    s_step = 0.5
+    step = 0.5
     ref_path_length = 10.0
-    ref_path_s = numpy.arange(0,ref_path_length,s_step)
+    ref_path_s = numpy.arange(0,ref_path_length,step)
     if(ref_path_s[-1] != ref_path_length):
         ref_path_s = numpy.append(ref_path_s,ref_path_length)
 
@@ -315,9 +129,38 @@ if __name__ == "__main__":
     ref_path_boundary_lower_l[14] = -4.0
     ref_path_boundary_upper_l[15] = 0.0
     ref_path_boundary_lower_l[15] = -4.0
+
+    ref_path_size = len(ref_path_s)
+    ref_path_s_step = [ref_path_s[i+1] - ref_path_s[i] for i in range(ref_path_size - 1)]
+    ref_path_l = [0.5 * (ref_path_boundary_lower_l[i] + ref_path_boundary_upper_l[i]) for i in range(ref_path_size)]
     
-    path_optimize = PieceJerkPathOptimize(ref_path_s,ref_path_boundary_upper_l,\
-                                          ref_path_boundary_lower_l, ref_path_kappa, w_l,\
-                                          w_dl, w_ddl, w_dddl, w_ref_l, wheel_base, max_delta,\
-                                          max_delta_rate)
+    # bound
+    l_lower_bound = ref_path_boundary_lower_l
+    l_upper_bound = ref_path_boundary_upper_l
+
+    # dl = (1 - kappa * l) * tan(delta_theta)
+    dl_bound = math.tan(numpy.deg2rad(30))
+    dl_lower_bound = [-dl_bound for i in range(ref_path_size)]
+    dl_upper_bound = [dl_bound for i in range(ref_path_size)]
+
+    # ddl = tan(max_delta)/wheel_base - k_r
+    ddl_bound = (math.tan(max_delta)/wheel_base - 0.0)
+    ddl_lower_bound = [-ddl_bound for i in range(ref_path_size)]
+    ddl_upper_bound = [ddl_bound for i in range(ref_path_size)]
+
+    # dddl
+    dddl_bound = max_delta_rate / wheel_base / 2.0
+    dddl_lower_bound = [-dddl_bound for i in range(ref_path_size)]
+    dddl_upper_bound = [dddl_bound for i in range(ref_path_size)]
+
+    path_optimize = PieceJerkPathOptimize(len(ref_path_s))
+    path_optimize.SetWeight(w_l, w_dl, w_ddl, w_dddl, w_ref_l)
+    path_optimize.SetReferencePathS(ref_path_s)
+    path_optimize.SetReferenceX(ref_path_l)
+    path_optimize.SetStep(ref_path_s_step)
+    path_optimize.SetXBound(l_upper_bound, l_lower_bound)
+    path_optimize.SetDXBound(dl_upper_bound, dl_lower_bound)
+    path_optimize.SetDDXBound(ddl_upper_bound, ddl_lower_bound)
+    path_optimize.SetDDDXBound(dddl_upper_bound, dddl_lower_bound)
     path_optimize.Optimize()
+    path_optimize.VizResult()
